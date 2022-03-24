d20b1d46ab364b2f032220f3453a96ea6b1b9242
mverist <98953572+mverist@users.noreply.github.com>
2022-03-24 17:28:42 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+I would like to introduce you the future 
+Just kidding 
+It's just an "idea" i had  while i was listening to this song " https://www.youtube.com/watch?v=waMkFIzvDpE "
+Enjoy!!!
+# ToeatOrnotToeat
---
+++ fastfoodtoss.py
@@ -0,0 +1,11 @@
+#python3
+
+from random import randrange
+print(f"\nThe results are: ")
+print (randrage(2))
+print(f"\nAnd you will")
+print(f"\n1=not order and eat fast food")
+print(f"\n0=order and eat fast food")
+#cute code for cute people
+#https://www.youtube.com/watch?v=waMkFIzvDpE ( a song i remember similar to this code lines [toss a coin])
+
